e5c899f7bb075e7a9aa8e587e82236516341819b
Mark J Perryman <appledad05@gmail.com>
2022-05-18 08:17:11 -0600

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Mark Perryman on 5/18/22.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
